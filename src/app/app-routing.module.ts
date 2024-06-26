191b5b57c6d2391a061e063bf9f2837030673d33
suicaandrei <suica.andrei@gmail.com>
2024-06-26 13:56:25 +0300

    Added about-us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'servicii',
     component: ServicesComponent,
+  },
+  {
+    path: 'despre-noi',
+    component: AboutUsComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ServicesComponent } from './services/services.component';
 import { SharedModule } from './shared/shared.module';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent, ServicesComponent],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    ServicesComponent,
+    AboutUsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
